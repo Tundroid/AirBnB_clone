adb2053b2899c6477930af6c49f6a877386c6ed6
Shamainb <shamainbhengesa60@gmail.com>
2023-12-10 14:09:37 -0800

    update

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,3 +3,12 @@
 
 import unittest
 from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+    """Tests for the Amenity class"""
+
+      def test_default_name(self):
+        """Test if the default name is an empty string"""
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+
