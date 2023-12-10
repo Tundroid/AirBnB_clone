ae768b0fef947d09378df76fb58dec6ea877c8f5
Shamainb <shamainbhengesa60@gmail.com>
2023-12-10 13:26:15 -0800

    updating

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -9,3 +9,10 @@ class TestCity(unittest.TestCase):
     def test_attributes(self):
         self.assertEqual(self.city.state_id, "")
         self.assertEqual(self.city.name, "")
+
+    def test_instance(self):
+        self.assertIsInstance(self.city, City)
+        self.assertIsInstance(self.city, BaseModel)
+
+if __name__ == '__main__':
+    unittest.main()
