219347d76f79c73a26315da196d61d83ed805108
Shamainb <shamainbhengesa60@gmail.com>
2023-12-10 13:24:37 -0800

    clone

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,3 +5,7 @@ class TestCity(unittest.TestCase):
 
     def setUp(self):
         self.city = City()
+
+    def test_attributes(self):
+        self.assertEqual(self.city.state_id, "")
+        self.assertEqual(self.city.name, "")
