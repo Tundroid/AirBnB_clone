a68d6f30d86bf0bf42eb4ab90f1650b6b045e51e
Shamainb <shamainbhengesa60@gmail.com>
2023-12-10 14:31:48 -0800

    update

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -3,3 +3,12 @@
 
 import unittest
 from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review class implementation"""
+
+    def __init__(self, place_id="", user_id="", text=""):
+        super().__init__()
+        self.place_id = place_id
+        self.user_id = user_id
+        self.text = text
