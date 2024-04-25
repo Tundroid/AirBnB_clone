e7881bdd862c146bc18739b7213cdca86f107f91
Tundroid <felixkimbu@outlook.com>
2024-04-25 10:27:23 +0100

    Fixed loading...

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,11 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            self.id = kwargs["id"]
-            self.created_at = datetime.fromisoformat(kwargs["created_at"])
-            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
+            for key, value in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -32,7 +34,6 @@ class BaseModel:
     def to_dict(self):
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
-        my_dict["id"] = self.id
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
