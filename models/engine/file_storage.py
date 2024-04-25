8d61472456459e802c1f9b6e4aaa6a49508260a0
Tundroid <felixkimbu@outlook.com>
2024-04-25 11:34:47 +0100

    Fixed loading...3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,9 +14,9 @@ from models.review import Review
 class FileStorage():
     """FileStorage Class Implementation"""
 
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         return self.__objects
