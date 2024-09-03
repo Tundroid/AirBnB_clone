869e738b473491b65f4147799c655ca6f069f602
Tundroid <felixkimbu@outlook.com>
2024-09-03 14:39:20 +0100

    Fixed Basemodel failing test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,11 +6,6 @@ import datetime
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
         self.model = BaseModel()
-        self.kwargs = {
-            'id': 'e49ada22-140b-4f58-8a1d-6af38c5dda19',
-            'created_at': '2024-09-03T12:00:00',
-            'updated_at': '2024-09-03T12:00:00'
-        }
 
     def test_init_without_kwargs(self):
         self.assertTrue(hasattr(self.model, 'id'))
@@ -21,10 +16,15 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(self.model.updated_at, datetime.datetime)
 
     def test_init_with_kwargs(self):
-        model = BaseModel(**self.kwargs)
-        self.assertEqual(model.id, self.kwargs['id'])
-        self.assertEqual(model.created_at, datetime.datetime.fromisoformat(self.kwargs['created_at']))
-        self.assertEqual(model.updated_at, datetime.datetime.fromisoformat(self.kwargs['updated_at']))
+        kwargs = {
+            'id': 'e49ada22-140b-4f58-8a1d-6af38c5dda19',
+            'created_at': '2024-09-03T12:00:00',
+            'updated_at': '2024-09-03T12:00:00'
+        }
+        model = BaseModel(**kwargs)
+        self.assertEqual(model.id, 'e49ada22-140b-4f58-8a1d-6af38c5dda19')
+        self.assertEqual(model.created_at, datetime.datetime.fromisoformat('2024-09-03T12:00:00'))
+        self.assertEqual(model.updated_at, datetime.datetime.fromisoformat('2024-09-03T12:00:00'))
 
     def test_str_representation(self):
         self.assertEqual(str(self.model), f"[BaseModel] ({self.model.id}) {self.model.__dict__}")
@@ -35,12 +35,12 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(self.model.updated_at, updated_at)
 
     def test_to_dict(self):
-        model_dict = BaseModel(**self.kwargs).to_dict()
+        model_dict = self.model.to_dict()
         self.assertIsInstance(model_dict, dict)
         self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertEqual(model_dict['id'], self.kwargs['id'])
-        self.assertEqual(model_dict['created_at'], self.kwargs['created_at'])
-        self.assertEqual(model_dict['updated_at'], self.kwargs['updated_at'])
+        self.assertEqual(model_dict['id'], self.model.id)
+        self.assertEqual(model_dict['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], self.model.updated_at.isoformat())
 
 
 if __name__ == '__main__':
